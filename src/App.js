2bc1f0a9b94addc20e08b8084ae47bbfc1e4b259
Emerson Gonçalves <emerson.u.g@gmail.com>
2021-01-06 19:03:58 -0300

    Ajuste no background da página

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, {useState} from 'react';
 import Styled from 'styled-components';
 
+const Body = Styled.body`
+  margin: 0px;
+  padding: 0px;
+  background-color: #0D1117;
+  height: 100vh;
+`
+
 const Card = Styled.div`
   background-color: #f6f6f6;
   width: 500px;
@@ -27,22 +34,24 @@ function App() {
 
   return (
     <div className="App">
-      <Card>
-        <h2>Calculadora de Gorjeta</h2>
-        <p>Quanto deu a conta?</p>
-        <Input type='number' placeholder="Digite o valor" value={valor} onChange={(e)=> setValor(e.target.value)} />
-        <p>Qual a porcentagem da gorjeta?</p>
-        <Input type='number' placeholder="Digite a porcentagem" value={porcentagem} onChange={(e)=>setPorcentagem(e.target.value)} />
-        {valor > 0 &&
-          <>
-            <h4>Sub-total: R${valor}</h4> 
-            <h4>Gorjeta: R${porcentagem}</h4>
-            <Valor>
-              <h3>Total: R${(parseFloat(valor) + ((valor * porcentagem) / 100))}</h3>
-            </Valor>
-          </>
-        }
-      </Card>
+      <Body>
+        <Card>
+          <h2>Calculadora de Gorjeta</h2>
+          <p>Quanto deu a conta?</p>
+          <Input type='number' placeholder="Digite o valor" value={valor} onChange={(e)=> setValor(e.target.value)} />
+          <p>Qual a porcentagem da gorjeta?</p>
+          <Input type='number' placeholder="Digite a porcentagem" value={porcentagem} onChange={(e)=>setPorcentagem(e.target.value)} />
+          {valor > 0 &&
+            <>
+              <h4>Sub-total: R${valor}</h4> 
+              <h4>Gorjeta: R${porcentagem}</h4>
+              <Valor>
+                <h3>Total: R${(parseFloat(valor) + ((valor * porcentagem) / 100))}</h3>
+              </Valor>
+            </>
+          }
+        </Card>
+      </Body>
     </div>
   );
 }
